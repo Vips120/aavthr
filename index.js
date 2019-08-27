9521a48cab0a66fcc9490dd47b55e88cfca31567
vips120 <vipulsingh0000@gmail.com>
2019-08-27 13:39:19 +0530

    #expressapi

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+// console.log(express);
+const app = express();
+app.use(express.json());
+  let users = [{
+      id:1,
+      name:'asrar bhai'
+  },
+ {
+     id:2,
+     name:'adil bhai'
+ },
+ {
+     id:3,
+     name:'Teja bhai'
+ },
+ {
+     id:4,
+     name:'vishal dada'
+ }
+];
+
+
+app.get('/api/user',(req,res) => {
+    // res.send('hello user');
+    res.send(users);
+});
+
+app.get('/api/user/:id',(req,res) => {
+    // let id = req.params.id;
+    let u = users.find(data => data.id === parseInt(req.params.id));
+    if(!u) {return res.status(403).send('invalid id')}
+    res.send(u);
+});
+
+app.post('/api/user/newuser', (req,res) => {
+    let newData = {
+        id:users.length + 1,
+        name:req.body.name
+    };
+    users.push(newData);
+    res.send({message:'created new user data',
+       data:users
+})
+});
+
+app.put('/api/user/updateuser/:id' ,(req,res) => {
+    let u = users.find(data => data.id === parseInt(req.params.id));
+    if(!u) {return res.status(403).send('invalid id')}
+    u.name = req.body.name;
+    res.send(u);
+});
+
+app.delete('/api/user/removeuser/:id' ,(req,res) => {
+    let u = users.find(data => data.id === parseInt(req.params.id));
+    if(!u) {return res.status(403).send('invalid id')};
+    let index = users.indexOf(u);
+    let removeid = users.splice(index,1);
+    res.send('rip the user id! see you next time :(');
+})
+
+
+app.listen(4000,() => {
+    console.log('server working on port number 4000');
+});
